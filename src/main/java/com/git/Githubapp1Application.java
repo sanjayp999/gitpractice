9820ceaea757df671c0cfa71055c67655f2cb950
sanjayp <saisanjayp99@gmail.com>
2023-09-14 17:15:01 +0530

    first commit

---
+++ src/main/java/com/git/Githubapp1Application.java
@@ -0,0 +1,13 @@
+package com.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Githubapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Githubapp1Application.class, args);
+	}
+
+}
